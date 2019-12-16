def7207b4b3937def65229df5074861d5f107f8f
Bessie Torp <btorp@oa-f3.21-school.ru>
2019-12-16 16:50:42 +0300

    удалил build, out

--- src/main/java/ru/sberbank/labs/lab1/MyHashMap.java
+++ src/main/java/ru/sberbank/labs/lab1/MyHashMap.java
@@ -1,10 +1,28 @@
 package ru.sberbank.labs.lab1;
 
+import java.util.Set;
+
 public class MyHashMap implements IntMap{
     private static int size = 0;
     private void addSize() {
         this.size++;
     }
+
+    @Override
+    public Object get(int i) {
+        return null;
+    }
+
+    @Override
+    public Object put(int key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object remove(int i) {
+        return null;
+    }
+
     @Override
     public int size(){return this.size;}
     @Override
@@ -12,5 +30,25 @@ public class MyHashMap implements IntMap{
         if (this.size() > 0)
             return false;
         return true;
+    }
+
+    @Override
+    public boolean containsKey(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object o) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<IntEntry<Person>> entrySet() {
+        return null;
     }
 }
