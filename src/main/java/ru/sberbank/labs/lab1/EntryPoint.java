20db479d86501d21daceb636adbfadf3eeaf41a2
Bessie Torp <btorp@oa-f3.21-school.ru>
2019-12-16 15:21:16 +0300

    сделано 10, не включая 9

--- src/main/java/ru/sberbank/labs/lab1/EntryPoint.java
+++ src/main/java/ru/sberbank/labs/lab1/EntryPoint.java
@@ -61,42 +61,68 @@ public class EntryPoint {
         return children;
     }
 
-//    public static Long[] longGenerator(int size){
-//        Long[] digs = new Long[size];
-//        Set<Integer> existingNumbers = new Set<Integer>;
-//        return digs;
-//    }
+    public static Integer[] integerGenerator(int size) {
+        Random rand = new Random();
+        HashSet<Integer> digsSet = new HashSet<>(size);
+        int temp = 0;
+        for (int i = 0; i < size; i++) {
+            while(digsSet.contains(temp)){
+                temp = rand.nextInt(Integer.MAX_VALUE) - rand.nextInt(Integer.MAX_VALUE) - 1;
+            }
+            digsSet.add(temp);
+        }
+        return digsSet.toArray(Integer[]::new);
+    }
+    public  static  float getConsumedTimeInMilliseconds(long finish, long start){
+        return (finish - start) / (1000 * 1000);
+    }
     public static void main(String[] args) {
         // 1. Реализуйте интерфейс Comparable в классе Person //done
+        if (!personTest())
+            System.out.println("Person compare doesnt working");
+        else
+            System.out.println("Person compare ok");
         // 2. Создайте массив Person размерностью 100_000
         // 3. Сгенерируйте 100_000 экземпляров Person с помощью генератора случайных чисел
         // Для случайной генерации строковых данных в читаемом виде можно использовать
         // предопределенные массивы строк firstname[] и lastname[]
         // 4. Поместите сгенерированные данные в массив
-        Person[] persons = personsGenerator(1000000);
+        int size = 1000000;
+        Person[] persons = personsGenerator(size);
         // 5. Создайте массив Long размерностью 100_000
         // 6. Сгенерируйте 100_000 уникальных случайных чисел в диапазоне (Long.MAX_VALUE, Long.MAX_VALUE)
         // Используйте Set<Integer> для обеспечения уникальности создаваемых чисел
-//        Long[] digs = longGenerator(1000000);
+        Integer[] digs = integerGenerator(size);
         // 7. Создайте экземпляры HashMap и TreMap
+
         Map<Integer, Person> hashMap = new HashMap<>();
         Map<Integer, Person> treeMap = new TreeMap<>();
         // 8. Используя созданный массив Integer[] и Person[] поместите
         // данные в созданные структуры, измерьте время, затраченное на запись данных для TreeMap и HashMap
+        long start = System.nanoTime();
+        for (int i = 0; i < size; i++)
+            hashMap.put(digs[i], persons[i]);
+        long finish = System.nanoTime();
+        System.out.printf("time for add hashMap in milliseconds:  %f\n", getConsumedTimeInMilliseconds(finish, start));
+        start = System.nanoTime();
+        treeMap.putAll(hashMap);
+        finish = System.nanoTime();
+        System.out.printf("time for add treeMap in milliseconds:  %f\n", getConsumedTimeInMilliseconds(finish, start));
         // 9. Используя массив Integer[] извлеките данные из структур данных, измерьте время, затраченное на чтение
+
         // 10. Улучшите показатели HashMap на запись, изучив доступные конструкторы HashMap
+        Map<Integer, Person> hashMapWithCap = new HashMap<>(size);
+        start = System.nanoTime();
+        for (int i = 0; i < size; i++)
+            hashMapWithCap.put(digs[i], persons[i]);
+        finish = System.nanoTime();
+        System.out.printf("time for add hashMapWithCap add in milliseconds:  %f\n", getConsumedTimeInMilliseconds(finish, start));
         // 11. Создайте свою собственную структуру данных, реализовав интерфейс IntMap
         // Изучите реализацию соответствующих методов в стандартной реализации HashMap, используйте их
         // как руководство к действию.
         // 12. Измерьте производительность созданной структуры данных.
 
         // P.S. Опционально: реализовать все измерительные процедуры в junit тестах
-        //Тесты 1 пункт
-        if (!personTest())
-            System.out.println("Person compare doesnt working");
-        else
-            System.out.println("Person compare ok");
-        //Тесты 2 пункт
     }
 }
 
